73823c24be55bbb49d9c84b14f2f0b217d552f86
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-05-19 06:55:34 +0200

    readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Cellma
+> Cell based texteditor.
+
+## Philosophy
+> Cellma is not your usual <b>texteditor</b>, Cellma is your
+> <b>friendly dictator</b>.
+> Cellma will not allow more than <b>80 characters horizontally</b>.
+> Even though she might be <b>rough</b>, she is also very <b>playful</b>,
+> <b>hackable</b> and
+> <b>customizable</b>.
+> Cellma is a <b>colorful</b> <b>dictator</b>.
+> She is a <b>texteditor</b>.
+
+### Please note
+> Please note that this texteditor is currently just a proof of concept and
+> is not of any use at this state.
+
+> There is a lot of problems and issues that I haven't taken care of yet,
+> I have just been playing around with what it can do.
+
+> For example, there is some array-index-out-of-bounds problems when moving
+> the cursor in the editor. It's a pretty easy fix but I just haven't taken
+> care of it yet.
+
+> You may also note that I am coming from a Python, Java, PHP, Javascript world
+> and I might have broken every C/C++ rule there is, in this project.
+
+### TODO
+* Document saving
+* Proper document importing (This currently works as seen in the "DocumentScene.h")
+* Line numbers
+* Commands
+* Plugin support
+* C/C++ MAKE FILE!!
+* More awesome stuff...
+
+> Please feel free to contribute!
--- src/DocumentScene.h
+++ src/DocumentScene.h
@@ -78,25 +78,29 @@ class DocumentScene: public Scene {
         }
 
         void initialize(float delta) {
+            /*
+             * What I am doing here is basically importing a file into
+             * the editor. Uncomment it to see.
+             */
             /*fstream fin;
-            fin.open("src/file.txt", ios::in);
+              fin.open("src/file.txt", ios::in);
 
-            char my_character ;
-            int xx = 0;
-            int yy = 0;
+              char my_character ;
+              int xx = 0;
+              int yy = 0;
 
-            while (!fin.eof() ) {
-                string sym(1, my_character); 
-                std::string str(sym.c_str());
-                cells[xx][yy]->character = str;
-                //cells.insert(cells.begin(), new Cell(xx*CELL_SIZE, yy*CELL_SIZE, str));
-                fin.get(my_character);
+              while (!fin.eof() ) {
+              string sym(1, my_character); 
+              std::string str(sym.c_str());
+              cells[xx][yy]->character = str;
+            //cells.insert(cells.begin(), new Cell(xx*CELL_SIZE, yy*CELL_SIZE, str));
+            fin.get(my_character);
 
-                xx = xx+1;
-                if (my_character == '\n' || xx >= 80){
-                    yy = yy+1;
-                    xx = 0;
-                }
+            xx = xx+1;
+            if (my_character == '\n' || xx >= 80){
+            yy = yy+1;
+            xx = 0;
+            }
             }*/
         }
 };
