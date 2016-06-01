d00639a7b0eb0ae7e4b2e1d985720ede5a2ef1cb
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-06-01 20:30:53 +0200

    commandparser can now parse arguments

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -1,11 +1,21 @@
 #include "CommandParser.h"
-
+#include <sstream>
+#include <vector>
 
 CommandParser::CommandParser() {
 }
 
 bool CommandParser::parse(string command) {
-    cout << command << endl;
+    std::stringstream test(command);
+    std::string segment;
+    std::vector<std::string> args;
+
+    while(std::getline(test, segment, ' '))
+    {
+        args.push_back(segment);
+        cout << segment << endl;
+    } 
+
     this->lastCommand = command;
     return true;
 }
